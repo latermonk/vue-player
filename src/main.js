d17a234d1c347956d6ab757ecced0927489a6b30
latermonk <iback.china@gmail.com>
2021-07-07 15:43:59 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueRouter from 'vue-router'
 
 
 Vue.use(ElementUI)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
