d3b46c7aed9cb08f2c867dad2da527f3f484c5da
latermonk <iback.china@gmail.com>
2021-07-07 14:59:46 +0800

    add nginx install option

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <endlessLove msg="endlessLove, I'm coming"/>
-    <Header></Header>
+    <Header></Header> <br/><br/>
+    <h1>安装目标机器</h1>
+    <Nginx></Nginx>
   </div>
 
 
@@ -15,10 +17,12 @@
 
 import endlessLove from './components/endlessLove'
 import Header from './components/header'
+import Nginx from "@/components/nginx"
 
 export default {
   name: 'App',
   components: {
+    Nginx,
     endlessLove,
     Header,
 
---
+++ src/components/nginx.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-select v-model="value" placeholder="请选择">
+    <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: "nginx",
+  data() {
+    return {
+      options: [{
+        value: '选项1',
+        label: 'Ubuntu 18.04 x64'
+      }, {
+        value: '选项2',
+        label: 'Ubuntu 18.04 x32'
+      }, {
+        value: '选项3',
+        label: 'CentOS x64'
+      }, {
+        value: '选项4',
+        label: 'CentOS x32'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
