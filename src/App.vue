8d280275af785a0d75f3b512d21835d21f0b7ac8
latermonk <iback.china@gmail.com>
2021-07-07 13:43:08 +0800

    add componetns endlessLove

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <endlessLove msg="endlessLove, I'm coming"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import endlessLove from './components/endlessLove'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    endlessLove,
+
   }
 }
 </script>
---
+++ src/components/endlessLove.vue
@@ -0,0 +1,13 @@
+<template>
+<div>endlessLove , Make The  world works better again</div>
+</template>
+
+<script>
+export default {
+  name: "endlessLove"
+}
+</script>
+
+<style scoped>
+
+</style>
